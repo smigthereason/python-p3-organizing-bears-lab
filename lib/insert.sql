6671eb8ede28425f419ceaa074028dff56254e39
victor@victor <victor.dmaina@gmail.com>
2024-06-04 23:57:35 +0300

    create table,insert to table,select from table

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1, "Mr. Chocolate", 24, "F", "White", "Social", 1);
+INSERT INTO bears VALUES(2, "Rowdy", 22, "M", "Black", "Tough", 0);
+INSERT INTO bears VALUES(3, "Tabitha", 27, "F", "Black", "hardworking", 1);
+INSERT INTO bears VALUES(4, "Sergeant Brown", 34, "M", "Black", "Tough", 1);
+INSERT INTO bears VALUES(5, "Mellisa", 20, "F", "White", "Happy", 0);
+INSERT INTO bears VALUES(6, "Grinch", 19, "M", "White", "Funny", 1);
+INSERT INTO bears VALUES(7, "Wendy", 25, "F", "Black", "Optimistic", 1);
+INSERT INTO bears VALUES(8, NULL, 8, "F", "White", "jovial", 0);
